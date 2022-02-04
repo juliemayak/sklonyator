fe743e770842775fa1169de9c0d03ccd8106467e
Julie <Julia@MacBook-Pro-Julie.local>
2022-02-04 10:52:30 +0300

    minor css fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ function Nav(): JSX.Element {
       <hr className="border-stone-50 w-1/3 " />
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-stone-50 font-comforter text-6xl px-5 py-2">Склонятор</h1>
-        <h2 className="text-xl font-oswald px-5 py-2 text-stone-50">
+        <h2 className="text-xl font-oswald px-5 py-2 text-stone-50 text-center">
           Сайт для склонения существительных по падежам
         </h2>
       </div>
